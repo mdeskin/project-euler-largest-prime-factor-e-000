2d925845c792ca024cec6afe7f11afaa9bc5095c
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-07 17:06:30 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+def largest_prime_factor(input)
+
+end
+
+# increment divisor
+# is divisor prime?
+#1f divisor is prime, try to divide value
+# practical limit of half?
