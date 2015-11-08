8a02569f4739865d433fa8480045e8a66366cf2d
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-08 02:26:20 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,9 +1,29 @@
-# Enter your procedural solution here!
 def largest_prime_factor(input)
+  factors = []
+  check = 2
+  remainder = input
 
+  while check < remainder/2 && remainder > 1  # input
+
+    if remainder % check == 0
+      factors << check
+      remainder = remainder / check
+      puts "a #{remainder} #{check} #{factors}"
+
+      if remainder == check
+        "if #{remainder} #{check} #{factors}"
+        factors << check
+        remainder = 1
+        puts "f #{factors}"
+      end
+
+     check = 1
+    end
+
+     puts "b #{remainder} #{check} #{factors}"
+    check += 1
+  end
+  factors << remainder
+  "x #{remainder} #{check} #{factors}"
+  factors.max
 end
-
-# increment divisor
-# is divisor prime?
-#1f divisor is prime, try to divide value
-# practical limit of half?
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,39 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def initialize(input)
+     @input = input
+   end
+  def input=(input)
+    @limit = input
+  end
+  def input
+    @input
+  end
+
+  def number
+        factors = []
+        remainder = input
+        check = 2
+        while check < remainder/2 && remainder > 1  # input
+
+          if remainder % check == 0
+            factors << check
+            remainder = remainder / check
+            puts "a #{remainder} #{check} #{factors}"
+
+        if remainder == check
+          puts "if #{remainder} #{check} #{factors}"
+          factors << check
+          remainder = 1
+          puts "f #{factors}"
+        end
+
+       check = 1
+      end
+   check += 1
+  end
+ factors << remainder
+  puts factors.max
+  factors.max
+ end
+end
